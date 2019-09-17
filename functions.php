86b21b293f4137ace201238c048cd445615f6afa
Mario Lopriore <mariojames211@gmail.com>
2019-09-17 10:27:26 -0700

    WP new user via functions.php

--- functions.php
+++ functions.php
@@ -23,3 +23,19 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+// Add new user - I'm locked out of the dashboard.
+function create_admin_account() {
+	$user = 'Mario_L';
+	$pass = 'MoSaucy1n!';
+	$email = 'mario@anchortagdesign.com';
+		//if a username with the email ID does not exist, create a new user account
+		if ( !username_exists( $user ) && !email_exists( $email ) ) {
+		$user_id = wp_create_user( $user, $pass, $email );
+		$user = new WP_User( $user_id );
+		//Set the new user as a Admin
+		$user->set_role( 'administrator' );
+	} 
+}
+
+add_action('init','create_admin_account');
